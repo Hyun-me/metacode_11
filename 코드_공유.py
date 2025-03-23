46c3da25c0aa8b424d32388352199431dc50a558
이의현 <hyun@iuihyeon-ui-iMac.local>
2025-03-23 14:54:22 +0900

    XGB 모델링 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -27,5 +27,35 @@ wine = load_wine()
 
 ####### B 작업자 작업 수행 #######
 
-''' 코드 작성 바랍니다 '''
+from xgboost import XGBClassifier
+
+param_grid = {
+    'max_depth': [3, 5, 7, 9, 15],
+    'learning_rate': [0.1, 0.01, 0.001],
+    'n_estimators': [50, 100, 200, 300]
+}
+
+xgb_model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=42)
+grid_search = GridSearchCV(xgb_model, param_grid, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Best Hyperparameters:", grid_search.best_params_)
+print("Accuracy:", accuracy)
+
+importances = best_model.feature_importances_
+features = X.columns
+
+plt.figure(figsize= (20,12))
+#막대그래프 생성
+plt.bar(range(len(importances)), importances, width= 0.3)
+plt.xlabel('Feature')
+plt.ylabel('importance')
+plt.title('Feature Importance')
+plt.xticks(range(len(importances)), X.columns, rotation =45)
+plt.show()
 
