e2e616fb733d22ffacebfc03247f1aad60b09b61
이의현 <hyun@iuihyeon-ui-iMac.local>
2025-03-23 14:40:06 +0900

    하이퍼파라미터 조정

--- 코드_공유.py
+++ 코드_공유.py
@@ -16,12 +16,51 @@ wine = load_wine()
 # target은 'target' 컬럼만을 대상으로 합니다.
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
-''' 코드 작성 바랍니다 '''
+X = pd.DataFrame(wine.data, columns=wine.feature_names)
+y = wine.target
 
+# 학습데이터와 테스트 데이터로 분리 (80% 학습, 20% 테스트)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 ####### A 작업자 작업 수행 #######
+# 하이퍼파라미터 설정
+param_grid = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [2, 3, 4, 5],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
 
-''' 코드 작성 바랍니다 '''
+# Grid Search 를 활용한 하이퍼파라미터 튜닝
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# Decision Tree 모델 생성 및 학습
+dt = DecisionTreeClassifier(random_state=42)
+grid_search = GridSearchCV(dt, param_grid, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+# 최적의 하이퍼파라미터 학습
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("\n Accuracy:", accuracy)
+print("\n Best Hyperparams", grid_search.best_params_)
+
+# Feature Importance 시각화
+importances = best_model.feature_importances_
+features = X.columns
+
+plt.figure(figsize=(20, 12))
+plt.bar(range(len(importances)), importances, width= 0.3)
+plt.xlabel('Feature')
+plt.ylabel('importance')
+plt.title('Feature Importance')
+plt.xticks(range(len(importances)), X.columns, rotation =45)
+plt.show()
 
 
 
